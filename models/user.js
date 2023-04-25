87eaf57462b0a32d6cf1c6a3c9bea8f1a934c556
darkstellar16 <itsmeshubham2799@gmail.com>
2023-04-25 15:53:34 +0530

    added database models

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");    //required mongoose
+
+
+const userSchema = new mongoose.Schema({        //created a schema which tells how the data will be saved in database..
+
+    name:
+    {
+        type: String,
+        required: true,
+        minlength: 3
+    },
+    email:
+    {
+        type: String,
+        required: true,
+        unique: true
+    },
+    mobile:
+    {
+        type: Number,
+        required: true,
+        minlength: 10,
+        maxlenght: 10
+    },
+    password:
+    {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+})
+
+
+const userInfo = mongoose.model('user', userSchema);      //mongoose model
+
+module.exports = userInfo;
