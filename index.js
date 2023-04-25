65274b909e17a996635e2731fca8f40dbd154506
darkstellar16 <itsmeshubham2799@gmail.com>
2023-04-25 15:39:01 +0530

    added database config and initialzation

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-require("dotenv").config;
+require("dotenv").config;     //for environment variables..
 
-const express = require("express");
+const express = require("express");         //required express
 const app = express();
 
-const bodyParser = require("body-parser");
+
+const datbaseConnection = require("./config/db.js");    // for database
+datbaseConnection();
+
+
+
+const bodyParser = require("body-parser");         //for body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;                 //for host
 
 
 
-// app.listen(port, () => {
-//     console.log("Server is up and Running");
-// })
+app.listen(port, () => {                                
+    console.log("Server is up and Running");
+})
